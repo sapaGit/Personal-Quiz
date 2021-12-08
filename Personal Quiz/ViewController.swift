d0bc75d27be78693782506e61f859e9e6a609b8d
Sapa <mosevaly@gmail.com>
2021-12-08 07:38:09 +0300

    Initial Commit

---
+++ Personal Quiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Personal Quiz
+//
+//  Created by Sergey Pavlov on 08.12.2021.
+//  Copyright © 2021 Sergey Pavlov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
